cc4670321a6ae2099eca6080092c24c458595eb6
xnoctra <mnv2_dev@outlook.com>
2025-03-28 09:55:29 -0400

    games init + patch + prettier

--- public/assets/js/formatDesc.js
+++ public/assets/js/formatDesc.js
@@ -1,8 +1,8 @@
 // Enables max character limit on desc for games & apps
 var i;
-var divs = document.getElementsByClassName("description");
+var divs = document.getElementsByClassName('description');
 for (i = 0; i < divs.length; i++) {
-  if (divs[i].className == "description") {
-    divs[i].innerHTML = divs[i].innerHTML.substring(0, 45) + "...";
+  if (divs[i].className == 'description') {
+    divs[i].innerHTML = divs[i].innerHTML.substring(0, 45) + '...';
   }
 }
